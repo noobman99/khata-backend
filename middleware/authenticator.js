c4a2380511e45c7c43d50985c86ad86b8792ad27
Parth Patil <psp90804@gmail.com>
2024-05-04 22:09:57 +0530

    fixed error codes in authenticator

--- middleware/authenticator.js
+++ middleware/authenticator.js
@@ -5,9 +5,9 @@ const authenticator = async (req, res, next) => {
   let { authorization: authToken } = req.headers;
 
   if (!authToken) {
-    res.status(401).json({
+    res.status(403).json({
       success: false,
-      error: "Please login to access this.",
+      error: "Unauthorized access.",
     });
   }
 
@@ -23,15 +23,15 @@ const authenticator = async (req, res, next) => {
     if (err.name === "TokenExpiredError") {
       res.status(801).json({
         success: false,
-        error: "Login Session expired. Please login again.",
+        error: "Login Session expired.",
       });
     } else if (err.name === "JsonWebTokenError") {
       res.status(800).json({
         success: false,
-        error: "Invalid credentials. Please login again.",
+        error: "Invalid credentials.",
       });
     } else {
-      res.status(401).json({
+      res.status(500).json({
         success: false,
         error: "Server could not process your request at this time.",
       });
